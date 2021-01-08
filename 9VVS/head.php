f8d5edd8bf3b79ea74687146090114279a84e34f
Albert <36663617+sakhibullin@users.noreply.github.com>
2021-01-08 14:17:03 +0300

    Add files via upload

---
+++ 9VVS/head.php
@@ -0,0 +1,33 @@
+
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Ledger&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body class="body">
+	<div class="container">
+		<header>
+			<h2 class="hellow">9 Спец. школе ВВС посвещяем</h2>
+			<div class="line"></div>
+			<div class="button_better">	
+				<a href="#" class="qestion">Вопросы? Напишите</a>
+			</div>	
+			<img src="img/Logo.png" alt="Logo" class="logo">
+
+			<div class="links">
+				<div class="instgroup">
+					<a href="https://www.instagram.com/lyceum_kfu/" target="_blank "><img src="img/icons/inst.png" alt="" class="inst__ico"></a>
+					<a href="https://www.instagram.com/lyceum_kfu/" target="_blank" class="inst">lyceum_kfu</a>
+				</div>
+				<div class="vkgroup">
+					<a href="https://vk.com/lobachevskylyceum" target="_blank "><img src="img/icons/vk.png" alt="" class="vk__ico"></a>
+					<a href="https://vk.com/lobachevskylyceum" target="_blank "class="vk">vk.com/lobachevskylyceum</a>
+				</div>
+				<a href="https://kpfu.ru/liceum" target="_blank" class="site">liceum.kpfu.ru</a>
+			</div>
+		</header>
+	</div>
+</body>
